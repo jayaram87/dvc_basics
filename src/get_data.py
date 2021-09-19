93472f98a883b47410b33814d39777e3758df20d
Jayaram Kottimugalur <jayaramraja1987@gmail.com>
2021-09-19 15:51:00 +0530

    stage 1

--- src/get_data.py
+++ src/get_data.py
@@ -3,17 +3,16 @@ import yaml
 import pandas as pd
 import argparse
 
-def read_parmas(config_path):
+def read_params(config_path):
     with open(config_path, 'r') as f:
         config = yaml.safe_load(f)
     return config
 
 def get_data(config_path):
-    config = read_parmas(config_path)
+    config = read_params(config_path)
     #print(config)
     data = config['data_source']['s3_source']
     df = pd.read_csv(data, sep=',', encoding='utf-8').iloc[:, 1:]
-    print(df.head())
     return df
 
 if __name__ == '__main__':
