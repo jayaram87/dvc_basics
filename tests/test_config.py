6d7a249e16e34e2031209699b81814f0373cea73
Jayaram Kottimugalur <jayaramraja1987@gmail.com>
2021-09-19 23:15:34 +0530

    custom error

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,3 +1,13 @@
+import pytest
+
+class NotInRange(Exception):
+    def __init__(self, msg='value not in range'):
+        self.msg = msg
+        super().__init__(self.msg)
+
 def test_generic():
     a, b = 2,2
+    with pytest.raises(NotInRange):
+        if a not in range(10,15):
+            raise NotInRange
     assert a == b
