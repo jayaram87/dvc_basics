2098e9427889ccd29e52cd2901a7e8b2d2022d19
Jayaram Kottimugalur <jayaramraja1987@gmail.com>
2021-09-19 21:57:58 +0530

    tox updated

---
+++ build/lib/src/get_data.py
@@ -0,0 +1,22 @@
+import os
+import yaml
+import pandas as pd
+import argparse
+
+def read_params(config_path):
+    with open(config_path, 'r') as f:
+        config = yaml.safe_load(f)
+    return config
+
+def get_data(config_path):
+    config = read_params(config_path)
+    #print(config)
+    data = config['data_source']['s3_source']
+    df = pd.read_csv(data, sep=',', encoding='utf-8').iloc[:, 1:]
+    return df
+
+if __name__ == '__main__':
+    args = argparse.ArgumentParser()
+    args.add_argument('--config', default='params.yaml')
+    parse_args = args.parse_args()
+    get_data(config_path=parse_args.config)
---
+++ setup.py
@@ -0,0 +1,4 @@
+from setuptools import setup, find_packages
+
+setup(name='src', version="0.0.1", description="sales package", packages=find_packages(), license='MIT', author='jay')
+
